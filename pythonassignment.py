4dce1bb247815645620e03bba82ab7359ba3672b
Shaikzaheer174 <68013910+Shaikzaheer174@users.noreply.github.com>
2020-07-15 15:47:53 +0530

    Add files via upload

---
+++ pythonassignment.py
@@ -0,0 +1,15 @@
+#my assignment 
+#the assignment is "contact form"
+print("contact form")
+name=input("hello sir,please enter your name :")
+age=int(input("please enter your age :"))
+course=input("please enter your course name :")
+contact=int(input("please enter your contact number :"))
+print("")
+print("the name you had entered is :",name)
+print("the age you had entered is :",age)
+print("the course you had entered is :",course)
+print("the contact number you had entered is :",contact)
+print("")
+confirmation=bool(input(" note: the information you had provided is true or false :"))
+

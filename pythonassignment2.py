a185b8de3733e9b137b08c6c5a921281f7117232
Shaikzaheer174 <68013910+Shaikzaheer174@users.noreply.github.com>
2020-07-16 15:30:52 +0530

    Add files via upload

---
+++ pythonassignment2.py
@@ -0,0 +1,13 @@
+#assignment
+#topic: decision making statements
+"""
+the assignment is...Subtraction between 2 numbers
+requirements: Always get a positive number
+"""
+number1=int(input("please enter your first number : "))
+number2=int(input("please enter your second number : "))
+if number1>number2:
+     print(f"subtraction of {number2} from {number1} is :",number1-number2)
+else: 
+   print(f"subtraction of {number1} from {number2} is :",number2-number1)
+

ac08b3504213a3ba791f7d87754d2aa364792740
Shaikzaheer174 <68013910+Shaikzaheer174@users.noreply.github.com>
2020-07-21 21:05:27 +0530

    Add files via upload

---
+++ pythonassignment3.py
@@ -0,0 +1,14 @@
+#assignment
+"""assignment is to solve or debug the code while entering the correct username and password 
+it's getting error"""
+
+uname="zaheer"
+password="zaheer123"
+username=input("please enter ur username: ")
+password=input("please enter your password: ")
+if username==uname and password==password:
+    print(f"hello {uname} our team welcomes u,have a nice day.")
+else:
+     print("please enter your correct username and password")
+     
+     

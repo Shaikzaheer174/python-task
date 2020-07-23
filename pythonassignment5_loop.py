26926ff133a767df99f97a6a511c38383b2d2924
Shaikzaheer174 <68013910+Shaikzaheer174@users.noreply.github.com>
2020-07-23 14:08:32 +0530

    Add files via upload

---
+++ pythonassignment5_loop.py
@@ -0,0 +1,14 @@
+#assignment5
+"""we have to writedown how many times it loop the execution
+"""
+count=0
+number = 50616
+while number<151515100:
+    print("my name is zaheer")
+    number+=15194
+    count+=1
+print("my loop is terminated")
+print(f"my loop was executed upto {count} times")
+
+
+

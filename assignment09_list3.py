5368661382d512bf5063548c72e35c8b05422ef9
Shaikzaheer174 <68013910+Shaikzaheer174@users.noreply.github.com>
2020-07-28 16:51:33 +0530

    Add files via upload

---
+++ assignment09_list3.py
@@ -0,0 +1,18 @@
+#assignment
+#assume that i  have a list, inside a list  we have show  how many times a number exist in a list.
+
+
+name=['aman', 'preet', 'aman', 'preet', 'deep', 'aman', 'deep', 'deep', 'deep', 'aman', 'aman']
+print(name)
+print(type(name))
+
+#to print aman
+print("the name aman is founded in a given list",name.count("aman"),"times")
+
+
+#to print deep
+print("the name deep is founded in a given list",name.count("deep"),"times")
+
+
+#to print preet
+print("the name preet is founded in a given list",name.count("preet"),"times")
